c6b0fe738ebb33f9b17219ab040faa9607de7386
mokolodi1 <mokolodi1@gmail.com>
2016-02-11 18:16:08 -0800

    removed app part

--- config/teo/run.sh
+++
@@ -1,7 +0,0 @@
-export MONGO_URL="mongodb://localhost:27017/MedBook"
-
-if [ -z "$1" ]; then
-    meteor --port 3000 --settings ~/work/medbook/sample-groups/config/teo/settings.json
-else
-    meteor $1 --port 3000 --settings ~/work/medbook/sample-groups/config/teo/settings.json
-fi
