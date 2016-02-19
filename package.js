3aa760d02a650e631bba8a16746f9fd5ba056a71
mokolodi1 <mokolodi1@gmail.com>
2016-02-19 13:00:39 -0800

    bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'medbook:sample-group-widget',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: 'a sample group widget',
   // URL to the Git repository containing the source code for this package.
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
   api.use([
     "medbook:namespace@0.0.2",
     "medbook:primary-collections@0.0.16",
-    "medbook:collaborations@2.3.7",
+    "medbook:collaborations@2.3.9",
     "mokolodi1:helpers@0.0.10",
     "sacha:spin@2.3.1",
     "twbs:bootstrap@3.3.6",
